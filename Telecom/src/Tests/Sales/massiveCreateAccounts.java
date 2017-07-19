025620a9101485b530f39727bb616fc6d3e58bdb
Ezequiel Vargas <ezequiel.vargas@xappia.com>
2017-07-19 10:55:37 -0300

    New order

---
+++ Telecom/src/Tests/Sales/massiveCreateAccounts.java
@@ -0,0 +1,30 @@
+package Tests.Sales;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class massiveCreateAccounts  {
+	
+	public static void main(String[] args) throws IOException {
+	
+	File src=new File("C:\\Users\\Sofia Chardin\\Downloads\\poi-3.17-beta1");
+	
+	FileInputStream fis=new FileInputStream(src);
+	
+	XSSFWorkbook wb=new XSSFWorkbook(fis);
+	
+	XSSFSheet sheet1 = wb.getSheetAt(0);
+	
+	String data0 = sheet1.getRow(0).getCell(0).getStringCellValue();
+	
+	wb.close();
+	
+	}
+	
+
+}
---
+++ Telecom/src/Tests/Sales/newOrder.java
@@ -0,0 +1,16 @@
+package Tests.Sales;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+import Pages.login.Login;
+
+
+public class newOrder {
+	
+	private WebDriver driver;
+	
+	@Test
+	
+
+	
+}
