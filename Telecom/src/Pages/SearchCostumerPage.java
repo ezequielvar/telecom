cd93a5438b56b50924b22f08e5857da4794ced94
Ezequiel Vargas <ezequiel.vargas@xappia.com>
2017-08-01 16:19:45 -0300

    Casos de alta de contacto

--- Telecom/src/Pages/SearchCostumerPage.java
+++ Telecom/src/Pages/SearchCostumerPage.java
@@ -9,9 +9,23 @@ public class SearchCostumerPage {
 		
 		WebDriver driver;
 		
-		@FindBy (how = How.ID,using = "DocumentType")
-		WebElement DocumentType;
+		@FindBy (how = How.ID,using = "ContactName")
+		WebElement ContactName;
 		
+		@FindBy (how = How.ID,using = "PhoneNumber")
+		WebElement PhoneNumber;
+	
+		@FindBy (how = How.ID,using = "	AccountName")
+		WebElement 	AccountName;
 		
-
+		@FindBy (how = How.ID,using = "	DocumentType")
+		WebElement 	DocumentType;
+		
+		@FindBy (how = How.ID,using = "	DocumentNumber")
+		WebElement 	DocumentNumber;
+		
+		@FindBy (how = How.ID,using = "	Email")
+		WebElement 	Email;
+		
+			
 	}
--- Telecom/src/Tests/newContact.java
+++ Telecom/src/Tests/newContact.java
@@ -1,5 +1,7 @@
 package Tests;
 
+import org.openqa.selenium.By;
+
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -37,13 +39,36 @@ public class newContact extends ContactSearch {
 	public void createdNewValidContact()
 	{
 		searchContact(DNI, DocValue[0], genero[0]);
-			
+		if (driver.findElement(By.className("slds-select ng-touched ng-dirty ng-valid-parse ng-not-empty ng-valid ng-valid-required")) == null)
+		{
+			System.out.println("Caso ");
+		}
 	}
 	public void createdNewInvalidContact()
 	{
 			
 		searchContact(DNI, DocValue[2], genero[1]);
 		
+		if (driver.findElement(By.className("slds-select ng-touched ng-dirty ng-valid-parse ng-empty ng-invalid ng-invalid-required")) == null);
+		{
+			System.out.println("Caso ");
+		}
+	}
+	public void verifiedFieldDNI()
+	{
+		driver.findElement(By.id("document")).sendKeys("123");;
+		if (driver.findElement(By.className("slds-select ng-touched ng-dirty ng-valid-parse ng-empty ng-invalid ng-invalid-required")) == null)
+		{
+			driver.findElement(By.id("document")).clear();
+			driver.findElement(By.id("document")).sendKeys("1234567894");
+		}else
+		{
+			System.out.println("No valida valores menor a 7 digitos en el campo dni");
+		}
+		if(driver.findElement(By.className("slds-select ng-touched ng-dirty ng-valid-parse ng-empty ng-invalid ng-invalid-required")) == null)
+		{
+			System.out.println("No valida valores mayor a 9 digitos en el campo dni");
+		}
 	}
 
 
