ecb6249f74188a9265075f9ac9c8008e06163134
Leonardo Jerez <leonardo.jerez@xappia.com>
2017-08-02 12:20:00 -0300

    Mi setconexion

--- Telecom/src/Pages/setConexion.java
+++ Telecom/src/Pages/setConexion.java
@@ -15,13 +15,13 @@ import org.testng.annotations.Test;
 public class setConexion{
 		static WebDriver driver;
 
-		@BeforeTest
-		public static void setUp() throws MalformedURLException{
-		DesiredCapabilities capability = DesiredCapabilities.chrome();
-		capability.setBrowserName("chrome");
-		capability.setPlatform(Platform.WINDOWS);
-		driver = new RemoteWebDriver(new URL("http://10.249.37.243:5557/wd/hub"), capability);
-		}
+		//@BeforeTest
+		//public static void setUp() throws MalformedURLException{
+		//DesiredCapabilities capability = DesiredCapabilities.chrome();
+		//capability.setBrowserName("chrome");
+		//capability.setPlatform(Platform.WINDOWS);
+		//driver = new RemoteWebDriver(new URL("http://10.249.37.243:5557/wd/hub"), capability);
+	//	}
 
 		@AfterTest
 		public void kill(){
@@ -30,13 +30,22 @@ public class setConexion{
 
 		@Test
 		public void SimpleTest() {
-			driver.get("http://www.google.com");
+			driver.get("http://www.google.com");	
 		}
 		
 		public static WebDriver setupPablo() {
 			System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
 		    ChromeOptions options = new ChromeOptions();
 		    options.addArguments("user-data-dir=C:\\Users\\pablo\\AppData\\Local\\Google\\Chrome\\User Data");
+		    options.addArguments("start-maximized");
+		    driver = new ChromeDriver(options);
+		    return driver;
+		}
+		
+		public static WebDriver setupLeo() {
+			System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		    ChromeOptions options = new ChromeOptions();
+		    options.addArguments("user-data-dir=C:\\Users\\Xappia\\AppData\\Local\\Google\\Chrome\\User Data");
 		    options.addArguments("start-maximized");
 		    driver = new ChromeDriver(options);
 		    return driver;
