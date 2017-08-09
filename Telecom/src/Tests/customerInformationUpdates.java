a6f265d2e3f68b76ff3f6a3dc2c983839f725709
Pablo <pablo.spector@xappia.com>
2017-08-09 17:34:08 -0300

    TS7176_modifyTwoFieldsWhichDontTriggerIdentityValidationProcess, TS7177_modifyThreeFieldsWhichTriggerIdentityValidationProcess, TS7153_verifyBirthDateHasValidDateFormat

--- Telecom/src/Tests/customerInformationUpdates.java
+++ Telecom/src/Tests/customerInformationUpdates.java
@@ -18,7 +18,7 @@ import org.testng.annotations.Test;
 import Pages.customerInformation;
 import Pages.setConexion;
 
-public class costumerInformationUpdates extends TestBase {
+public class customerInformationUpdates extends TestBase {
 	
 	private WebDriver driver;
 
@@ -66,6 +66,9 @@ public class costumerInformationUpdates extends TestBase {
 		accounts.get(0).click();
 		try {Thread.sleep(3000);} catch (InterruptedException ex) {Thread.currentThread().interrupt();}
 		driver.switchTo().defaultContent();
+		try { if(driver.findElement(By.cssSelector(".x-layout-collapsed.x-layout-collapsed-west.x-layout-cmini-west")).isDisplayed()) {
+			driver.findElement(By.cssSelector(".x-layout-collapsed.x-layout-collapsed-west.x-layout-cmini-west")).click();
+			} } catch (org.openqa.selenium.NoSuchElementException e) {}
 		List<WebElement> frame1 = driver.findElements(By.tagName("iframe"));
 		driver.switchTo().frame(frame1.get(5));
 		List<WebElement> profileEdit = driver.findElements(By.className("profile-edit"));
@@ -96,6 +99,9 @@ public class costumerInformationUpdates extends TestBase {
 		accounts.get(0).click();
 		try {Thread.sleep(3000);} catch (InterruptedException ex) {Thread.currentThread().interrupt();}
 		driver.switchTo().defaultContent();
+		try { if(driver.findElement(By.cssSelector(".x-layout-collapsed.x-layout-collapsed-west.x-layout-cmini-west")).isDisplayed()) {
+			driver.findElement(By.cssSelector(".x-layout-collapsed.x-layout-collapsed-west.x-layout-cmini-west")).click();
+			} } catch (org.openqa.selenium.NoSuchElementException e) {}
 		List<WebElement> frame1 = driver.findElements(By.tagName("iframe"));
 		driver.switchTo().frame(frame1.get(5));
 		List<WebElement> profileEdit = driver.findElements(By.className("profile-edit"));
@@ -126,6 +132,9 @@ public class costumerInformationUpdates extends TestBase {
 		accounts.get(0).click();
 		try {Thread.sleep(3000);} catch (InterruptedException ex) {Thread.currentThread().interrupt();}
 		driver.switchTo().defaultContent();
+		try { if(driver.findElement(By.cssSelector(".x-layout-collapsed.x-layout-collapsed-west.x-layout-cmini-west")).isDisplayed()) {
+			driver.findElement(By.cssSelector(".x-layout-collapsed.x-layout-collapsed-west.x-layout-cmini-west")).click();
+			} } catch (org.openqa.selenium.NoSuchElementException e) {}
 		List<WebElement> frame1 = driver.findElements(By.tagName("iframe"));
 		driver.switchTo().frame(frame1.get(5));
 		List<WebElement> profileEdit = driver.findElements(By.className("profile-edit"));
@@ -156,6 +165,9 @@ public class costumerInformationUpdates extends TestBase {
 		accounts.get(0).click();
 		try {Thread.sleep(3000);} catch (InterruptedException ex) {Thread.currentThread().interrupt();}
 		driver.switchTo().defaultContent();
+		try { if(driver.findElement(By.cssSelector(".x-layout-collapsed.x-layout-collapsed-west.x-layout-cmini-west")).isDisplayed()) {
+			driver.findElement(By.cssSelector(".x-layout-collapsed.x-layout-collapsed-west.x-layout-cmini-west")).click();
+			} } catch (org.openqa.selenium.NoSuchElementException e) {}
 		List<WebElement> frame1 = driver.findElements(By.tagName("iframe"));
 		driver.switchTo().frame(frame1.get(5));
 		List<WebElement> profileEdit = driver.findElements(By.className("profile-edit"));
@@ -186,6 +198,9 @@ public class costumerInformationUpdates extends TestBase {
 		accounts.get(0).click();
 		try {Thread.sleep(3000);} catch (InterruptedException ex) {Thread.currentThread().interrupt();}
 		driver.switchTo().defaultContent();
+		try { if(driver.findElement(By.cssSelector(".x-layout-collapsed.x-layout-collapsed-west.x-layout-cmini-west")).isDisplayed()) {
+			driver.findElement(By.cssSelector(".x-layout-collapsed.x-layout-collapsed-west.x-layout-cmini-west")).click();
+			} } catch (org.openqa.selenium.NoSuchElementException e) {}
 		List<WebElement> frame1 = driver.findElements(By.tagName("iframe"));
 		driver.switchTo().frame(frame1.get(5));
 		List<WebElement> profileEdit = driver.findElements(By.className("profile-edit"));
@@ -216,6 +231,9 @@ public class costumerInformationUpdates extends TestBase {
 		accounts.get(0).click();
 		try {Thread.sleep(3000);} catch (InterruptedException ex) {Thread.currentThread().interrupt();}
 		driver.switchTo().defaultContent();
+		try { if(driver.findElement(By.cssSelector(".x-layout-collapsed.x-layout-collapsed-west.x-layout-cmini-west")).isDisplayed()) {
+			driver.findElement(By.cssSelector(".x-layout-collapsed.x-layout-collapsed-west.x-layout-cmini-west")).click();
+			} } catch (org.openqa.selenium.NoSuchElementException e) {}
 		List<WebElement> frame1 = driver.findElements(By.tagName("iframe"));
 		driver.switchTo().frame(frame1.get(5));
 		List<WebElement> profileEdit = driver.findElements(By.className("profile-edit"));
@@ -248,6 +266,9 @@ public class costumerInformationUpdates extends TestBase {
 		accounts.get(0).click();
 		driver.switchTo().defaultContent();
 		try {Thread.sleep(5000);} catch (InterruptedException ex) {Thread.currentThread().interrupt();}
+		try { if(driver.findElement(By.cssSelector(".x-layout-collapsed.x-layout-collapsed-west.x-layout-cmini-west")).isDisplayed()) {
+			driver.findElement(By.cssSelector(".x-layout-collapsed.x-layout-collapsed-west.x-layout-cmini-west")).click();
+			} } catch (org.openqa.selenium.NoSuchElementException e) {}
 		List<WebElement> frame1 = driver.findElements(By.tagName("iframe"));
 		driver.switchTo().frame(frame1.get(5));
 		waitFor(driver, (By.className("profile-edit")));		
@@ -276,5 +297,135 @@ public class costumerInformationUpdates extends TestBase {
 		waitFor(driver, (By.id("FirstName")));		
 		page.setDefaultValues();
 		waitFor(driver, (By.className("panel-heading")));		
+	}
+	
+	@Test	
+	public void TS7176_modifyTwoFieldsWhichDontTriggerIdentityValidationProcess() {
+		waitFor(driver, (By.cssSelector(".x-border-panel")));		
+		List<WebElement> mainTabs = driver.findElements(By.className("x-tab-strip-close"));
+		for (WebElement e : mainTabs) {
+				try {((JavascriptExecutor) driver).executeScript("arguments[0].click();", e);} catch (org.openqa.selenium.StaleElementReferenceException b) {}
+				}
+		List<WebElement> mainTabs1 = driver.findElements(By.className("x-tab-strip-close"));
+		((JavascriptExecutor) driver).executeScript("arguments[0].click();", mainTabs1.get(1));
+		goToLeftPanel(driver, "Cuentas");
+		WebElement frame0 = driver.findElement(By.tagName("iframe"));
+		driver.switchTo().frame(frame0);
+		waitFor(driver, (By.name("fcf")));	
+		Select field = new Select(driver.findElement(By.name("fcf")));
+		field.selectByVisibleText("Todas las cuentas");
+		waitFor(driver, (By.xpath("//*[text() = 'Adrian Tech']")));		
+		List<WebElement> accounts = driver.findElements(By.xpath("//*[text() = 'Adrian Tech']"));
+		accounts.get(0).click();
+		driver.switchTo().defaultContent();
+		try {Thread.sleep(5000);} catch (InterruptedException ex) {Thread.currentThread().interrupt();}
+		try { if(driver.findElement(By.cssSelector(".x-layout-collapsed.x-layout-collapsed-west.x-layout-cmini-west")).isDisplayed()) {
+			driver.findElement(By.cssSelector(".x-layout-collapsed.x-layout-collapsed-west.x-layout-cmini-west")).click();
+			} } catch (org.openqa.selenium.NoSuchElementException e) {}
+		List<WebElement> frame1 = driver.findElements(By.tagName("iframe"));
+		driver.switchTo().frame(frame1.get(5));
+		waitFor(driver, (By.className("profile-edit")));		
+		List<WebElement> profileEdit = driver.findElements(By.className("profile-edit"));
+		profileEdit.get(0).click();
+		driver.switchTo().defaultContent();
+		try {Thread.sleep(5000);} catch (InterruptedException ex) {Thread.currentThread().interrupt();}
+		List<WebElement> frame2 = driver.findElements(By.tagName("iframe"));		
+		driver.switchTo().frame(frame2.get(4));
+		waitFor(driver, (By.id("FirstName")));		
+		customerInformation page = new customerInformation(driver);
+		page.setTwoFieldsWhichDontTriggerIdentityValidationProcess();
+		waitFor(driver, (By.className("panel-heading")));		
+		List<WebElement> text = driver.findElements(By.className("panel-heading"));
+		Assert.assertTrue(text.get(0).getText().contains("Confirmación"));
+		driver.switchTo().defaultContent();
+		List<WebElement> tabs = driver.findElements(By.className("x-tab-strip-close"));
+		((JavascriptExecutor) driver).executeScript("arguments[0].click();", tabs.get(6));
+		List<WebElement> frame4 = driver.findElements(By.tagName("iframe"));
+		driver.switchTo().frame(frame4.get(5));
+		profileEdit.get(0).click();
+		driver.switchTo().defaultContent();
+		try {Thread.sleep(5000);} catch (InterruptedException ex) {Thread.currentThread().interrupt();}
+		List<WebElement> frame5 = driver.findElements(By.tagName("iframe"));		
+		driver.switchTo().frame(frame5.get(4));
+		waitFor(driver, (By.id("FirstName")));		
+		page.setDefaultValues();
+		waitFor(driver, (By.className("panel-heading")));		
+	}
+	
+	@Test	
+	public void TS7177_modifyThreeFieldsWhichTriggerIdentityValidationProcess() {
+		waitFor(driver, (By.cssSelector(".x-border-panel")));		
+		List<WebElement> mainTabs = driver.findElements(By.className("x-tab-strip-close"));
+		for (WebElement e : mainTabs) {
+				try {((JavascriptExecutor) driver).executeScript("arguments[0].click();", e);} catch (org.openqa.selenium.StaleElementReferenceException b) {}
+				}
+		List<WebElement> mainTabs1 = driver.findElements(By.className("x-tab-strip-close"));
+		((JavascriptExecutor) driver).executeScript("arguments[0].click();", mainTabs1.get(1));
+		goToLeftPanel(driver, "Cuentas");
+		WebElement frame0 = driver.findElement(By.tagName("iframe"));
+		driver.switchTo().frame(frame0);
+		waitFor(driver, (By.name("fcf")));	
+		Select field = new Select(driver.findElement(By.name("fcf")));
+		field.selectByVisibleText("Todas las cuentas");
+		waitFor(driver, (By.xpath("//*[text() = 'Adrian Tech']")));		
+		List<WebElement> accounts = driver.findElements(By.xpath("//*[text() = 'Adrian Tech']"));
+		accounts.get(0).click();
+		driver.switchTo().defaultContent();
+		try {Thread.sleep(7000);} catch (InterruptedException ex) {Thread.currentThread().interrupt();}
+		try { if(driver.findElement(By.cssSelector(".x-layout-collapsed.x-layout-collapsed-west.x-layout-cmini-west")).isDisplayed()) {
+			driver.findElement(By.cssSelector(".x-layout-collapsed.x-layout-collapsed-west.x-layout-cmini-west")).click();
+			} } catch (org.openqa.selenium.NoSuchElementException e) {}
+		List<WebElement> frame1 = driver.findElements(By.tagName("iframe"));
+		driver.switchTo().frame(frame1.get(5));
+		waitFor(driver, (By.className("profile-edit")));		
+		List<WebElement> profileEdit = driver.findElements(By.className("profile-edit"));
+		profileEdit.get(0).click();
+		driver.switchTo().defaultContent();
+		try {Thread.sleep(5000);} catch (InterruptedException ex) {Thread.currentThread().interrupt();}
+		List<WebElement> frame2 = driver.findElements(By.tagName("iframe"));		
+		driver.switchTo().frame(frame2.get(4));
+		waitFor(driver, (By.id("FirstName")));		
+		customerInformation page = new customerInformation(driver);
+		page.setThreeFieldsWhichTriggerIdentityValidationProcess();
+		try {Thread.sleep(5000);} catch (InterruptedException ex) {Thread.currentThread().interrupt();}
+		List<WebElement> text = driver.findElements(By.cssSelector(".ng-binding.ng-scope"));
+		Assert.assertEquals("No se pueden modificar Género, Número de documento y Fecha de Nacimiento al mismo tiempo.", text.get(3).getText());
+	}
+	
+	@Test	
+	public void TS7153_verifyBirthDateHasValidDateFormat() {
+		waitFor(driver, (By.cssSelector(".x-border-panel")));		
+		List<WebElement> mainTabs = driver.findElements(By.className("x-tab-strip-close"));
+		for (WebElement e : mainTabs) {
+				try {((JavascriptExecutor) driver).executeScript("arguments[0].click();", e);} catch (org.openqa.selenium.StaleElementReferenceException b) {}
+				}
+		List<WebElement> mainTabs1 = driver.findElements(By.className("x-tab-strip-close"));
+		((JavascriptExecutor) driver).executeScript("arguments[0].click();", mainTabs1.get(1));
+		goToLeftPanel(driver, "Cuentas");
+		WebElement frame0 = driver.findElement(By.tagName("iframe"));
+		driver.switchTo().frame(frame0);
+		waitFor(driver, (By.name("fcf")));	
+		Select field = new Select(driver.findElement(By.name("fcf")));
+		field.selectByVisibleText("Todas las cuentas");
+		waitFor(driver, (By.xpath("//*[text() = 'Adrian Tech']")));		
+		List<WebElement> accounts = driver.findElements(By.xpath("//*[text() = 'Adrian Tech']"));
+		accounts.get(0).click();
+		driver.switchTo().defaultContent();
+		try {Thread.sleep(7000);} catch (InterruptedException ex) {Thread.currentThread().interrupt();}
+		try { if(driver.findElement(By.cssSelector(".x-layout-collapsed.x-layout-collapsed-west.x-layout-cmini-west")).isDisplayed()) {
+		driver.findElement(By.cssSelector(".x-layout-collapsed.x-layout-collapsed-west.x-layout-cmini-west")).click();
+		} } catch (org.openqa.selenium.NoSuchElementException e) {}
+		List<WebElement> frame1 = driver.findElements(By.tagName("iframe"));
+		driver.switchTo().frame(frame1.get(5));
+		waitFor(driver, (By.className("profile-edit")));		
+		List<WebElement> profileEdit = driver.findElements(By.className("profile-edit"));
+		profileEdit.get(0).click();
+		driver.switchTo().defaultContent();
+		try {Thread.sleep(5000);} catch (InterruptedException ex) {Thread.currentThread().interrupt();}
+		List<WebElement> frame2 = driver.findElements(By.tagName("iframe"));		
+		driver.switchTo().frame(frame2.get(4));
+		waitFor(driver, (By.id("FirstName")));		
+		customerInformation page = new customerInformation(driver);
+		Assert.assertTrue(page.isBirthDateAValidDateFormat());
 	}
 }
