001ee839f6b30e5af1ca474e12b1a56605e87c63
Ezequiel Vargas <ezequiel.vargas@xappia.com>
2017-08-10 17:07:25 -0300

    cambios en NewContactInformation.java

--- Telecom/src/Tests/NewContactInformation.java
+++ Telecom/src/Tests/NewContactInformation.java
@@ -1,14 +1,17 @@
 package Tests;
 
 
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 
 import Pages.ContactInformation;
 import Pages.setConexion;
+import Tests.createdContact;
 
 public class NewContactInformation extends TestBase {
 	
@@ -24,83 +27,136 @@ public class NewContactInformation extends TestBase {
 		//super(driver);
 	//}
 	
-	//@AfterMethod
-	//public void tearDown() {
-	//	driver.close();
-	//}
+	@AfterMethod
+	public void tearDown() {
+		driver.close();
+	}
+	
 	@BeforeMethod
 	public void Setup() throws Exception
 	{
-		this.driver = setConexion.setupLeo();
-		//driver.get("https://c.cs14.visual.force.com/apex/taClientCreationProcess?id=a1zc0000003EQUYAA4&designerPreviewId=a1zc0000003EQUYAA4&previewEmbedded=true&tabKey=1499800882480#/OS/a1zc0000003EQUYAA4/scriptState/new/true/true");
+		this.driver = setConexion.setupEze();
+		login1(driver);
+		try {Thread.sleep(6000);} catch (InterruptedException ex) {Thread.currentThread().interrupt();}
+		createdContact nc = new createdContact();
+		nc.createdNewValidContact();
 	}
 	
-	
 	@Test
-	public void nonExistentContact()
+	public void TS6901_nonExistentContact()
 	{
 		ContactInformation page = new ContactInformation(driver);
 		page.setContactInformation(Name, LastName, DateOfBirthday, Email);
 		driver.findElement(By.id("UpdateContact")).click();
 	}
+	
 	@Test
-	public void numbersOnFieldLastName()
+	public void TS6920_verifyValidateContact()
 	{
-		driver.findElement(By.id("LastName")).sendKeys("123");
-		driver.findElement(By.cssSelector(".slds-form-element.vlc-flex.ng-scope.ng-valid-minlength.ng-valid-maxlength.ng-dirty.ng-valid-parse.ng-valid-required.ng-invalid.ng-invalid-pattern"));
+		ContactInformation page = new ContactInformation(driver);
+		page.setContactInformation(Name, LastName, DateOfBirthday, Email);
+		driver.findElement(By.id("Contact_nextBtn")).click();
+		driver.findElement(By.id("ValidationMethod0"));
 	}
 	@Test
-	public void numbersOnFieldName()
+	public void TS6909_numbersOnFieldLastName()
+	{
+		driver.findElement(By.id("LastName")).sendKeys("123");
+		driver.findElement(By.cssSelector(".slds-form-element.vlc-flex.ng-scope.ng-invalid.ng-valid-minlength.ng-valid-maxlength.ng-dirty.ng-valid-parse.ng-invalid-pattern.ng-valid-required"));
+	}
+	
+	@Test
+	public void TS6910_numbersOnFieldName()
 	{
 		driver.findElement(By.id("Name")).sendKeys("123");
-		driver.findElement(By.cssSelector(".slds-form-element.vlc-flex.ng-scope.ng-valid-minlength.ng-valid-maxlength.ng-dirty.ng-valid-parse.ng-valid-required.ng-invalid.ng-invalid-pattern"));	
+		driver.findElement(By.cssSelector(".slds-form-element.vlc-flex.ng-scope.ng-invalid.ng-valid-minlength.ng-valid-maxlength.ng-dirty.ng-valid-parse.ng-invalid-pattern.ng-valid-required"));	
 	}	
 	
 	@Test
-	public void Manualbirthdate() {		
+	public void TS6949_Manualbirthdate() 
+	{
 		driver.findElement(By.id("Birthdate")).sendKeys(DateOfBirthday);
 		driver.findElement(By.cssSelector(".slds-form-element.vlc-flex.ng-scope.ng-dirty.ng-valid-parse.ng-valid-required.ng-valid.ng-valid-valid"));
 	}
+	
 	@Test
-	public void Morethanfivedigits() {
+	public void TS6950_MoreThanFiveDigits()
+	{
 		driver.findElement(By.id("Birthdate")).sendKeys(DateOfBirthday +"4");
 		driver.findElement(By.cssSelector(".slds-form-element.vlc-flex.ng-scope.ng-dirty.ng-valid-parse.ng-valid-required.ng-invalid.ng-invalid-valid"));
 	}
+	
 	@Test
-	public void birthdateletters() {
+	public void birthdateLetters() 
+	{
 		driver.findElement(By.id("Birthdate")).sendKeys("agosto");
 		driver.findElement(By.className(".slds-input ng-touched ng-dirty ng-valid-parse ng-not-empty ng-valid-required ng-invalid ng-invalid-valid"));
 	}
+	
 	@Test
-	public void birthdateOnemoreday() {
+	public void TS6951_birthdateOneMoreDay() 
+	{
 		driver.findElement(By.id("Birthdate")).sendKeys("32/08/1999");
 		driver.findElement(By.cssSelector(".slds-form-element.vlc-flex.ng-scope.ng-dirty.ng-valid-parse.ng-valid-required.ng-invalid.ng-invalid-valid"));
 	}
+	
 	@Test
-	public void birthdateDayminusone() {
+	public void birthdateDayminusone() 
+	{
 		driver.findElement(By.id("Birthdate")).sendKeys("00/08/1999");
 		driver.findElement(By.cssSelector(".slds-form-element.vlc-flex.ng-scope.ng-dirty.ng-valid-parse.ng-valid-required.ng-invalid.ng-invalid-valid"));
 	}
+	
 	@Test
-	public void birthdateOnemoremonth() {
+	public void TS6952_birthdateOneMoreMonth() 
+	{
 		driver.findElement(By.id("Birthdate")).sendKeys("22/13/1999");
 		driver.findElement(By.cssSelector(".slds-form-element.vlc-flex.ng-scope.ng-dirty.ng-valid-parse.ng-valid-required.ng-invalid.ng-invalid-valid"));
 	}
+	
 	@Test
-	public void birthdateMonthminusone() {
+	public void birthdateMonthminusone() 
+	{
 		driver.findElement(By.id("Birthdate")).sendKeys("22/00/1999");
 		driver.findElement(By.cssSelector(".slds-form-element.vlc-flex.ng-scope.ng-dirty.ng-valid-parse.ng-valid-required.ng-invalid.ng-invalid-valid"));
 	}
 	
 	@Test
-	public void NoMailCheckEmpty() {
+	public void TS6974_mailCheckEmpty() 
+	{
 		driver.findElement(By.cssSelector(".ng-valid.ng-touched.ng-dirty.ng-valid-parse.ng-empty"));
 	}
 	
-	public void NoMailCheckSelect () {
+	@Test
+	public void TS6941_noMailCheckSelect () 
+	{
 		driver.findElement(By.id("EmailCheck")).click();
 		driver.findElement(By.cssSelector(".ng-valid.ng-touched.ng-dirty.ng-valid-parse.ng-not-empty"));
 	}
 	
+	@Test
+	public void TS6934_mandatoryLastName()
+	{
+		driver.findElement(By.cssSelector(".slds-form-element.vlc-flex.ng-pristine.ng-scope.ng-valid-pattern.ng-invalid.ng-invalid-required.ng-valid-minlength.ng-valid-maxlength"));
+	}
+	
+	@Test
+	public void TS6936_mandatoryBirthDate()
+	{
+		driver.findElement(By.cssSelector(".slds-form-element.vlc-flex.ng-pristine.ng-scope.ng-invalid.ng-invalid-required"));
+	}
+	
+	@Test
+	public void TS6938_mandatoryName()
+	{
+		driver.findElement(By.cssSelector(".slds-form-element.vlc-flex.ng-pristine.ng-scope.ng-valid-pattern.ng-invalid.ng-invalid-required.ng-valid-minlength.ng-valid-maxlength"));
+	}
+	
+	@Test
+	public void TS6931_calendarBirthDate()
+	{
+		driver.findElement(By.id("Birthdate")).click();
+		driver.findElement(By.cssSelector(".datepicker.-bottom-left-.-from-bottom-.active"));
+	}
 }
 
