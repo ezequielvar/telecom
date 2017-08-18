7beb49e3d2e734b59355d3a7ab809077df3ff4ea
Ezequiel Vargas <ezequiel.vargas@xappia.com>
2017-08-18 10:32:13 -0300

    PanelServiceActive de CC

--- Telecom/src/Tests/PanelServiceActive.java
+++ Telecom/src/Tests/PanelServiceActive.java
@@ -2,6 +2,9 @@ package Tests;
 
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotEquals;
+import static org.testng.Assert.assertNotSame;
+import static org.testng.Assert.assertTrue;
 
 import java.util.List;
 import org.openqa.selenium.By;
@@ -34,7 +37,7 @@ public class PanelServiceActive extends TestBase {
 	
 	@AfterTest
 	public void tearDown2() {
-//		driver.close();	
+		driver.close();	
 	}
 	
 	@BeforeMethod
@@ -69,7 +72,7 @@ public class PanelServiceActive extends TestBase {
 		try {Thread.sleep(3000);} catch (InterruptedException ex) {Thread.currentThread().interrupt();}
 		List<WebElement> panel = driver.findElements(By.xpath("//*[text() = 'Servicios']"));
 		panel.get(0).click();
-		try {Thread.sleep(6000);} catch (InterruptedException ex) {Thread.currentThread().interrupt();}
+		try {Thread.sleep(3000);} catch (InterruptedException ex) {Thread.currentThread().interrupt();}
 		List<WebElement> frame1 = driver.findElements(By.tagName("iframe"));
 		driver.switchTo().frame(frame1.get(2));
 		driver.findElement(By.xpath("/html/body/span/div/div/ng-include/div/form/div/input")).sendKeys("inter");
@@ -100,7 +103,7 @@ public class PanelServiceActive extends TestBase {
 		try {Thread.sleep(3000);} catch (InterruptedException ex) {Thread.currentThread().interrupt();}
 		List<WebElement> panel = driver.findElements(By.xpath("//*[text() = 'Servicios']"));
 		panel.get(0).click();
-		try {Thread.sleep(6000);} catch (InterruptedException ex) {Thread.currentThread().interrupt();}
+		try {Thread.sleep(3000);} catch (InterruptedException ex) {Thread.currentThread().interrupt();}
 		List<WebElement> frame1 = driver.findElements(By.tagName("iframe"));
 		driver.switchTo().frame(frame1.get(2));
 		driver.findElement(By.xpath("/html/body/span/div/div/ng-include/div/form/div/input"));
@@ -128,10 +131,40 @@ public class PanelServiceActive extends TestBase {
 		try {Thread.sleep(3000);} catch (InterruptedException ex) {Thread.currentThread().interrupt();}
 		List<WebElement> panel = driver.findElements(By.xpath("//*[text() = 'Servicios']"));
 		panel.get(0).click();
-		try {Thread.sleep(6000);} catch (InterruptedException ex) {Thread.currentThread().interrupt();}
+		try {Thread.sleep(3000);} catch (InterruptedException ex) {Thread.currentThread().interrupt();}
 		List<WebElement> frame1 = driver.findElements(By.tagName("iframe"));
 		driver.switchTo().frame(frame1.get(2));
-		driver.findElement(By.xpath("/html/body/span/div/div/ng-include/div/div[2]/div[1]/ng-include/section/div[1]/div/h2"));
+		assertNotSame(driver.findElement(By.xpath("/html/body/span/div/div/ng-include/div/div[2]/div[1]/ng-include/section/div[1]/div/h2")).getText(), "NULL");
+	}
+	
+	@Test
+	public void TS7133_validationScroll(){
+		try {Thread.sleep(5000);} catch (InterruptedException ex) {Thread.currentThread().interrupt();}
+		try{ for(WebElement e : driver.findElements(By.className("x-tab-strip-close"))) {
+			((JavascriptExecutor) driver).executeScript("arguments[0].click();", e);
+		} } catch (org.openqa.selenium.StaleElementReferenceException e) {}
+		try {Thread.sleep(5000);} catch (InterruptedException ex) {Thread.currentThread().interrupt();}
+		goToLeftPanel(driver, "Cuentas");
+		try {Thread.sleep(3000);} catch (InterruptedException ex) {Thread.currentThread().interrupt();}
+		WebElement frame0 = driver.findElement(By.tagName("iframe"));
+		driver.switchTo().frame(frame0);
+		Select field = new Select(driver.findElement(By.name("fcf")));
+		field.selectByVisibleText("Todas las cuentas");
+		try {Thread.sleep(2000);} catch (InterruptedException ex) {Thread.currentThread().interrupt();}
+		List<WebElement> accounts = driver.findElements(By.xpath("//*[text() = 'Fernando Care']"));
+		accounts.get(0).click();
+		try {Thread.sleep(3000);} catch (InterruptedException ex) {Thread.currentThread().interrupt();}
+		driver.switchTo().defaultContent();
+		try {Thread.sleep(3000);} catch (InterruptedException ex) {Thread.currentThread().interrupt();}
+		List<WebElement> panel = driver.findElements(By.xpath("//*[text() = 'Servicios']"));
+		panel.get(0).click();
+		try {Thread.sleep(3000);} catch (InterruptedException ex) {Thread.currentThread().interrupt();}
+		List<WebElement> frame1 = driver.findElements(By.tagName("iframe"));
+		driver.switchTo().frame(frame1.get(2));
+		JavascriptExecutor javascript = (JavascriptExecutor) driver;
+		Boolean VertscrollStatus = (Boolean) javascript.executeScript("return document.documentElement.scrollHeight>document.documentElement.clientHeight;");
+		assertTrue(VertscrollStatus);
+		driver.switchTo().defaultContent();
 	}
 }
 	
