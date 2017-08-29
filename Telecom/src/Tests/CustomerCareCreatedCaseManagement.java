132351c3f81650be832017a486c32ff1ba950d2e
xavier <xaviersmathis@gmail.com>
2017-08-29 15:38:20 -0300

    Created CustomerCarecreatedCasesManagement Class.

--- Telecom/src/Tests/CustomerCareCreatedCaseManagement.java
+++ Telecom/src/Tests/CustomerCareCreatedCaseManagement.java
@@ -2,45 +2,32 @@ package Tests;
 
 
 import java.util.List;
-import java.util.Set;
 
-import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import Pages.CasePage;
-import Pages.Login;
+import Pages.CustomerCasesManager;
+import Pages.SelectCaseRegisterType;
 import Pages.setConexion;
 
-public class CustomerCareCaseManagement extends TestBase {
+public class CustomerCareCreatedCaseManagement extends TestBase {
 
 	private WebDriver driver;
-	
-	
-	@BeforeClass
-	public void init() throws Exception
-	{
-		this.driver = setConexion.setupEze();
-		try {Thread.sleep(5000);} catch (InterruptedException ex) {Thread.currentThread().interrupt();}
-		login(driver);
-		try {Thread.sleep(5000);} catch (InterruptedException ex) {Thread.currentThread().interrupt();}
-	}
 
-	@BeforeMethod
+	@BeforeTest
 	public void mainSteup() {
+		this.driver = setConexion.setupPablo();	
+		login(driver);
 		try {Thread.sleep(4000);} catch (InterruptedException ex) {Thread.currentThread().interrupt();}
 		if (!driver.getCurrentUrl().toString().equals("https://cs14.salesforce.com/console")){
 			driver.findElement(By.id("tsidLabel")).click();
@@ -55,7 +42,7 @@ public class CustomerCareCaseManagement extends TestBase {
 		List<WebElement> mainTabs1 = driver.findElements(By.className("x-tab-strip-close"));
 		((JavascriptExecutor) driver).executeScript("arguments[0].click();", mainTabs1.get(1));
 		try {Thread.sleep(10000);} catch (InterruptedException ex) {Thread.currentThread().interrupt();}	
-		goToLeftPanel(driver, "Casos");
+		goToLeftPanel2(driver, "Casos");
 		WebElement frame0 = driver.findElement(By.tagName("iframe"));
 		driver.switchTo().frame(frame0);
 		waitFor(driver, (By.name("fcf")));	
@@ -63,79 +50,43 @@ public class CustomerCareCaseManagement extends TestBase {
 		field.selectByVisibleText("Mis casos");
 		driver.switchTo().defaultContent();
 		try {Thread.sleep(10000);} catch (InterruptedException ex) {Thread.currentThread().interrupt();}
-		List<WebElement> frame1 = driver.findElements(By.tagName("iframe"));
-		driver.switchTo().frame(frame1.get(0));
-		
-		driver.findElement(By.name("newCase")).click();
 	}
 	
 	@AfterClass
 	public void tearDown() {
-		
 		driver.switchTo().defaultContent();
 		List<WebElement> mainTabs1 = driver.findElements(By.className("x-tab-strip-close"));
 		((JavascriptExecutor) driver).executeScript("arguments[0].click();", mainTabs1.get(1));
-		try {Thread.sleep(10000);} catch (InterruptedException ex) {Thread.currentThread().interrupt();}
-		
-		waitFor(driver, (By.className("x-toolbar-cell")));
-		List<WebElement> btn = driver.findElements(By.cssSelector(".x-btn-text"));
-		((JavascriptExecutor) driver).executeScript("arguments[0].click();", btn.get(5));
-		driver.close();	
-		}
-	
-	@AfterMethod
-	public void alert (){
-		driver.get("https://cs14.salesforce.com/console");
-		try{
-			Alert alert = driver.switchTo().alert();
-			System.out.println(alert.getText());
-		}catch(org.openqa.selenium.NoAlertPresentException e){}
+		try {Thread.sleep(7000);} catch (InterruptedException ex) {Thread.currentThread().interrupt();}
+		driver.close();
 	}
-
 	
-		@Test
-	public void TS7193_CaseRelatedFieldsValuesCanalClosing(){
-
-
-		try {Thread.sleep(10000);} catch (InterruptedException ex) {Thread.currentThread().interrupt();}
-		driver.switchTo().defaultContent();
-		List<WebElement> frame2 = driver.findElements(By.tagName("iframe"));
-		driver.switchTo().frame(frame2.get(1));
-		CasePage page = new CasePage(driver);
-		page.ValidChannelClosing();
+	@BeforeMethod
+	public void setup() throws Exception {	
 	}
 	
 	@Test
-	public void TS7090_CaseRelatedFieldsValuesSubArea(){
+	public void PFTA_76_CreatedAndDueTimeInHoursFormatCheck(){
+		
+		String dateWithHourPattern = "(\\d{2}\\/\\d{2}\\/\\d{4} \\d{2}:\\d{2})";
 
-		try {Thread.sleep(10000);} catch (InterruptedException ex) {Thread.currentThread().interrupt();}
+		try {Thread.sleep(5000);} catch (InterruptedException ex) {Thread.currentThread().interrupt();}
 		driver.switchTo().defaultContent();
+
+		CustomerCasesManager customerCasesManagerPage = new CustomerCasesManager(driver);
+
+		customerCasesManagerPage.clickCase("00001678");
+		CasePage page = new CasePage(driver);
+		try {Thread.sleep(5000);} catch (InterruptedException ex) {Thread.currentThread().interrupt();}
+		driver.switchTo().defaultContent();
+
 		List<WebElement> frame2 = driver.findElements(By.tagName("iframe"));
 		driver.switchTo().frame(frame2.get(1));
-		CasePage page = new CasePage(driver);
-		page.FieldsValuesSubArea();
+
+		Assert.assertTrue(page.getCaseDate().matches(dateWithHourPattern));
+		Assert.assertTrue(page.getCaseDueDate().matches(dateWithHourPattern));
 		
 	}
 	
-	@Test
-	public void TS7088_CaseRelatedFieldsValuesType(){
-
-
-		try {Thread.sleep(10000);} catch (InterruptedException ex) {Thread.currentThread().interrupt();}
-		driver.switchTo().defaultContent();
-		List<WebElement> frame2 = driver.findElements(By.tagName("iframe"));
-		driver.switchTo().frame(frame2.get(1));
-		CasePage page = new CasePage(driver);
-		page.FieldsValuesType();
-
-}
-	
-	
 		
-		
-	
-	
-	
-	
-	
 }
