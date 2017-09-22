e491bdf4b204e76001fbd6862b520bc58e766552
xavier <xaviersmathis@gmail.com>
2017-09-22 17:03:03 -0300

    fase1.

--- Telecom/src/Tests/diagnosis.java
+++ Telecom/src/Tests/diagnosis.java
@@ -215,7 +215,7 @@ public class diagnosis extends TestBase {
 	}
 	*/
 	
-	@Test
+	@Test(groups = "fase1")
 	public void TS0000_diagnosticInternetCheck() {
 		try {Thread.sleep(5000);} catch (InterruptedException ex) {Thread.currentThread().interrupt();}
 		try{ for(WebElement e : driver.findElements(By.className("x-tab-strip-close"))) {
@@ -263,7 +263,7 @@ public class diagnosis extends TestBase {
 		goToLeftPanel2(driver, "Cuentas");
 	}
 
-	@Test(groups="a")
+	@Test(groups = {"a", "fase1"})
 	public void TS6256_Boton_Ejecutar_Grisado_Chequeo() {
 		//actually checks if not visible, it ist greyed out.
 		try {Thread.sleep(5000);} catch (InterruptedException ex) {Thread.currentThread().interrupt();}
@@ -300,7 +300,7 @@ public class diagnosis extends TestBase {
 		goToLeftPanel2(driver, "Cuentas");
 	}
 	
-	@Test
+	@Test(groups = "fase1")
 	public void TS6269_Habilitado_Al_Seleccionar_Motivo_Y_Sintoma() {
 		try {Thread.sleep(5000);} catch (InterruptedException ex) {Thread.currentThread().interrupt();}
 		try{ for(WebElement e : driver.findElements(By.className("x-tab-strip-close"))) {
@@ -360,7 +360,7 @@ public class diagnosis extends TestBase {
 		goToLeftPanel2(driver, "Cuentas");
 	}
 
-	@Test
+	@Test(groups = "fase1")
 	public void TS6320_Visualizar_Motivo_de_Contacto_Y_Sintoma() {
 		try {Thread.sleep(5000);} catch (InterruptedException ex) {Thread.currentThread().interrupt();}
 		try{ for(WebElement e : driver.findElements(By.className("x-tab-strip-close"))) {
@@ -403,7 +403,7 @@ public class diagnosis extends TestBase {
 		goToLeftPanel2(driver, "Cuentas");
 	}
 
-	@Test
+	@Test(groups = "fase1")
 	public void TS6410_Ingreso_A_Tech_Care_Desde_La_Vista_360() {
 		try {Thread.sleep(5000);} catch (InterruptedException ex) {Thread.currentThread().interrupt();}
 		try{ for(WebElement e : driver.findElements(By.className("x-tab-strip-close"))) {
